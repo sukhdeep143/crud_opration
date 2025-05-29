050b6f865d61af459a652bbfbf8aac84805147e1
Sukhdeep Singh <sukhdeepsingh141414@gmail.com>
2025-05-30 00:48:22 +0530

    put and get

--- index.js
+++ index.js
@@ -7,16 +7,48 @@ const app = express()
 app.use(express.json());
 
 
+// app.post('/api/pokemon', (req, res)=> {
+//    res.send("Working")
+    
+
+// });
+
+
 app.get('/', (req, res)=>{
     res.send("Hello from backend, Is Working?? yes THIS is node API");
     
 });
 
+app.get("/api/products", async (req, res)=>{
+    try {
+        
+       const products = await product.find({})
+       res.status(201).json(products)
+    } catch (error) {
+
+        res.status(500).json({message: error.message})
+        
+    }
+});
+
+app.get("/api/products/:id", async (req, res)=>{
+    try {
+        const {id} = req.params;
+       const products = await product.findById(id)
+       res.status(201).json(products)
+    } catch (error) {
+
+        res.status(500).json({message: error.message})
+        
+    }
+});
+
+
 app.post('/api/products', async (req, res) => {
     try {
 
         const Product_Store = await product.create(req.body);
-        res.status(200).json(Product_Store)
+        res.status(201).json(Product_Store)
         
     } catch (error) {
         res.status(500).json({message: error.message})
--- models/product.model.js
+++ models/product.model.js
@@ -1,29 +1,28 @@
 const mongoose = require("mongoose");
 
-const productSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please enter the product name!!!"],
-  },
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter the product name!!!"],
+    },
 
-  quantity: {
-    type: Number,
-    required: [true, "Please enter the value of your product"],
-    default: 0,
-  },
+    quantity: {
+      type: Number,
+      required: [true, "Please enter the value of your product"],
+      default: 0,
+    },
 
-  price: {
-    type: Number,
-    required: true,
-    default: 0,
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
   },
-},
-    {
-        Timestamp: true
-    }
-
+  {
+    timestamps: true, // ✅ Corrected spelling
+  }
 );
-
 
 const Product = mongoose.model("product", productSchema);
 
