6b987603b657f3af9ce2250bde9d54803081e6a2
Sukhdeep Singh <sukhdeepsingh141414@gmail.com>
2025-05-31 22:12:48 +0530

    update

--- index.js
+++ index.js
@@ -1,70 +1,72 @@
-const express = require('express');
+const express = require("express");
 const mongoose = require("mongoose");
-const product = require("./models/product.model.js")
+const product = require("./models/product.model.js");
 
-const app = express()
+const app = express();
 
 app.use(express.json());
 
-
 // app.post('/api/pokemon', (req, res)=> {
 //    res.send("Working")
-    
 
 // });
 
-
-app.get('/', (req, res)=>{
-    res.send("Hello from backend, Is Working?? yes THIS is node API");
-    
+app.get("/", (req, res) => {
+  res.send("Hello from backend, Is Working?? yes THIS is node API");
 });
 
-app.get("/api/products", async (req, res)=>{
-    try {
-        
-       const products = await product.find({})
-       res.status(201).json(products)
-    } catch (error) {
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await product.find({});
+    res.status(201).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-        res.status(500).json({message: error.message})
-        
+app.put("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const updatedProduct = await Product.findByIdAndUpdate(id, req.body);
+
+    if (!updatedProduct) {
+      return res.status(404).json({ message: "Product not found" });
     }
+
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-app.get("/api/products/:id", async (req, res)=>{
-    try {
-        const {id} = req.params;
-       const products = await product.findById(id)
-       res.status(201).json(products)
-    } catch (error) {
-
-        res.status(500).json({message: error.message})
-        
-    }
+app.get("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const products = await product.findById(id);
+    res.status(201).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-
-app.post('/api/products', async (req, res) => {
-    try {
-
-        const Product_Store = await product.create(req.body);
-        res.status(201).json(Product_Store)
-        
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-    
+app.post("/api/products", async (req, res) => {
+  try {
+    const Product_Store = await product.create(req.body);
+    res.status(201).json(Product_Store);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-mongoose.connect("mongodb+srv://sukh:Sukhdeep2003@cluster0.r32l3jk.mongodb.net/").then(()=>{
+mongoose
+  .connect("mongodb+srv://sukh:Sukhdeep2003@cluster0.r32l3jk.mongodb.net/")
+  .then(() => {
     console.log("Connected to database");
-    app.listen(3000, ()=>{
-    console.log("We are listion on port 3000");
-    
-}); 
-    
-})
-.catch(()=>{
+    app.listen(3000, () => {
+      console.log("We are listion on port 3000");
+    });
+  })
+  .catch(() => {
     console.log("Failed to connect");
-    
-})
+  });
